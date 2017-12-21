e04dfac81128c13a78de58cbe645bb47e765966d
vexrax <joshuaf_111@live.com>
2017-12-20 20:36:12 -0500

    Added multiple enemy box animations.

---
+++ Assets/Scripts/EnemyCubEBehaviour.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCubEBehaviour : MonoBehaviour {
+
+    public Color color;
+    public float speed;
+    private bool isStarted;
+    private bool setup;
+    private enum scripts { upwardSwing, SwidewardSwing };
+    private scripts currentScript;
+
+    void Start ()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", color);
+        isStarted = false;
+        setup = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log(isStarted);
+        Debug.Log("pos y: " + transform.position.y);
+        Debug.Log("pos x: " + transform.position.x);
+        Debug.Log("Current State: " + currentScript);
+
+        if (!isStarted)
+        {
+            selectScript();
+            isStarted = true;
+            runScripts();
+        }
+        else
+        {
+            runScripts();
+        }
+    }
+    public void selectScript()
+    {
+        this.currentScript = (scripts)(new System.Random().Next(0, 2));
+    }
+    public void runScripts()
+    {
+        switch(currentScript)
+        {
+            case scripts.upwardSwing:
+                upwardSwingBox();
+                break;
+            case scripts.SwidewardSwing:
+                sidewardsSwingBox();
+                break;
+        }
+
+    }
+    public void boxSetup()
+    {
+        switch (currentScript)
+        {
+            case scripts.upwardSwing:
+                transform.SetPositionAndRotation(new Vector3(0, -9f, 0), new Quaternion());
+                setup = true;
+                break;
+            case scripts.SwidewardSwing:
+                transform.SetPositionAndRotation(new Vector3(4f, 0, 0), new Quaternion());
+                setup = true;
+                break;
+        }
+    }
+    public void upwardSwingBox()
+    {
+        if (!setup)
+            boxSetup();
+        transform.localScale = new Vector3(6f, 7f, 1f);
+        transform.Translate(new Vector3(0, 1 * speed));
+        if (transform.position.y >= 10f)
+        {
+            isStarted = false;
+            setup = false;
+        }
+    }
+    public void sidewardsSwingBox()
+    {
+        if (!setup)
+            boxSetup();
+        transform.localScale = new Vector3(1f, 10f, 1f);
+        transform.Translate(new Vector3(-1 * speed, 0));
+        if (transform.position.x <= -4f)
+        {
+            isStarted = false;
+            setup = false;
+        }
+    }
+}
---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl control;
+    public float health;
+    public int points;
+
+    private void Awake()
+    {
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start ()
+    {
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	void OnGUI ()
+    {
+        GUI.Label(new Rect(10, 10, 100, 30), "Health:" + health);
+        GUI.Label(new Rect(10, 40, 100, 30), "Points:" + points);
+    }
+
+    private void FixedUpdate()
+    {
+        var gObj = GameObject.Find("MainPlayer"); //hook this into a checker to check for coliding boxes. 
+        control.points += 1;
+    }
+}
